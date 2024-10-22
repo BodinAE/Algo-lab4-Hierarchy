a024e5f642d9c5c1336d3ac99dcb7d7570e6d74d
BodinAE <BodinAE@mer.ci.nsu.ru>
2024-10-22 13:10:38 +0700

    Add square

--- Algo-lab4-Hierarchy/Square.cs
+++ Algo-lab4-Hierarchy/Square.cs
@@ -3,48 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Algo_lab4_Hierarchy
 {
-    internal class Rectangle : Shape
+    internal class Square : Rectangle
     {
-        public Point Pos1 { get; set; }
-        public Point Pos2 { get; set; }
-        public Point Pos3 { get; set; }
-        public Point Pos4 { get; set; }
-        public Line Line1 { get; set; }
-        public Line Line2 { get; set; }
-        public Line Line3 { get; set; }
-        public Line Line4 { get; set; }
-        public decimal Area { get; set; }
-        public Rectangle(string name = "Rectangle", decimal x1 = 0, decimal y1 = 0, decimal x2 = 0, decimal y2 = 0)
+        decimal a { get; set; }
+        public Square(string name, Point pos, decimal a)
         {
+            this.a = a;
+            var x1 = pos.x;
+            var y1 = pos.y;
+            var x2 = pos.x + a;
+            var y2 = pos.y + a;
             Name = name;
             Pos1 = new Point(x1, y1);
             Pos2 = new Point(x1, y2);
             Pos3 = new Point(x2, y2);
             Pos4 = new Point(x2, y2);
-            Line1 = new Line($"{name}_L1",Pos1, Pos2);
+            Line1 = new Line($"{name}_L1", Pos1, Pos2);
             Line2 = new Line($"{name}_L2", Pos2, Pos3);
             Line3 = new Line($"{name}_L3", Pos3, Pos4);
             Line4 = new Line($"{name}_L4", Pos4, Pos1);
             MiddlePos = new Point((x1 + x2) / 2, (y1 + y2) / 2);
-            Area = Line1.Length * Line2.Length;
+            Area = a * a;
         }
+
         public void Print()
         {
-            Console.WriteLine($"Rectangle:\t{Name}");
+            Console.WriteLine($"Square:\t{Name}");
             MiddlePos.Print();
             Line1.Print();
             Line2.Print();
             Line3.Print();
             Line4.Print();
             Console.WriteLine($"Area:\t{Area}");
-            //Console.WriteLine($"Point A:\t{Pos1}\nPoint B:\t{Pos2}\nPoint C:\t{Pos3}\nPoint D:\t{Pos4}\nArea:\t{Area}");
         }
+
         public override string ToString()
         {
-            return $"Rectangle: {Name}\tPoint A: {Pos1}\tPoint C: {Pos3}";
+            return $"Square: {Name}\tLength: {a}\tArea: {Area}";
         }
     }
 }
